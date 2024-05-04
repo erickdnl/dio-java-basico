18c7446c6c42b35c4324a08e74a62bdf14239acd
Erick Daniel <derickanielgg@gmail.com>
2024-05-03 22:28:13 -0300

    add sistema-smart-tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,35 @@
+
+public class Usuario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SmartTv smartTv = new SmartTv();
+		
+		System.out.println("TV Ligada? " + smartTv.ligada);
+		System.out.println("Canal Atual: " + smartTv.canal);
+		System.out.println("Volume Atual: " + smartTv.volume);
+		
+		smartTv.ligar();
+		System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+		
+		smartTv.desligar();
+		System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+		
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.aumentarVolume();
+		System.out.println("Volume Atual: " + smartTv.volume);
+		
+		System.out.println("Canal Atual: " + smartTv.canal);
+		smartTv.aumentarCanal();
+		System.out.println("Canal Atual: " + smartTv.canal);
+		smartTv.diminuirCanal();
+		System.out.println("Canal Atual: " + smartTv.canal);
+		smartTv.mudarCanal(13);
+		System.out.println("Canal Atual: " + smartTv.canal);
+		
+	}
+
+}
